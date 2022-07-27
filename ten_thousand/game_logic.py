e8531f4a5a5dc55e59cdac09b46c518c022dd700
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-26 22:17:49 -0700

    quitter and one/done scripts finished

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,7 +1,10 @@
 from collections import Counter
 import random
 
+from pip import main
+
 class GameLogic:
+  
   def __init__(self):
     pass
 
@@ -91,10 +94,31 @@ class GameLogic:
 
   @staticmethod
   def roll_dice(dice):
+    print(f"Rolling {dice} dice...")
     roll = []
     for _ in range(dice):
       roll.append(random.randint(1, 6))
-    print(roll)
     result = tuple(roll)
-    print(result)
+    print(f" *** {result} ***") # TODO get rid of ()
     return result 
+
+
+
+
+
+print("Welcome to Ten Thousand")
+print("(y)es to play or (n)o to decline")
+wanna_play = input("> ")
+if wanna_play == "n":
+  print("OK. Maybe another time")
+  exit()
+if wanna_play == "y":
+  print("Starting round 1")
+  GameLogic.roll_dice(6)
+print("Enter dice to keep, or (q)uit:")
+keeps = input("> ")
+if keeps == "q":
+  print("Thanks for playing. You earned 0 points")
+  exit()
+
+
